f007bd0121b6f89d7064aa6c36b0f36af7ce84c8
Jan Van Riel <=>
2022-09-06 09:47:36 +0200

    exporting new classes

--- sdk.ts
+++ sdk.ts
@@ -7,5 +7,7 @@ export type { Filter, FilterExpression, custom } from './sdk/listObject.ts';
 export { AttributeType } from './sdk/structuredObject.ts';
 export { FileUploader } from './sdk/fileUploader.ts';
 export { type FileInfo } from './sdk/files.ts';
-
+export { StorageBag } from './sdk/storage_bag.ts';
+export { Folder } from './sdk/folder.ts';
+export { Twintag } from './sdk/twintag.ts';
 export { VERSION } from './version.ts';
